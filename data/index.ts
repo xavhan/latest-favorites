8e213501f0cc2d93fc3371a4a9dd2ada5c06bb01
Xavier Haniquaut <xavier.haniquaut@gmail.com>
2023-04-04 10:50:25 +0200

    add data

---
+++ data/index.ts
@@ -0,0 +1,7 @@
+export * from "./2022_09";
+export * from "./2022_10";
+export * from "./2022_11";
+export * from "./2022_12";
+export * from "./2023_01";
+export * from "./2023_02";
+export * from "./2023_03";
